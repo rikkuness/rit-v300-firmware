338a83c567e3c42cd27acba10c76ff4802fa7a8f
Darrian <darrian@robco-industries.org>
2025-09-07 23:36:37 +0100

    added hid implementation via ch9350

--- main.h
+++ main.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #define col_black 0x00
-#define col_grey 0x0C
+#define col_grey  0x0C
 #define col_white 0x0F
 
-void splash(void);
+void splash(char *extra);
 void terminal_app(void);
-int main(void);
+int  main(void);
+
+// USB host power enable pin
+#define USBH_POWER_EN 29
+
+// USB host pins
+#define USBH_DPLUS  32
+#define USBH_DMINUS 33
